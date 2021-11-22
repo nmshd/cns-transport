337391e967894a1078f4658d60f50fa9bc618225
jkoenig <julian.koenig@js-soft.com>
2021-11-22 15:53:26 +0100

    Revert "chore: disable proxy"
    This reverts commit 7c06cbe8cdd22315bb50a149a194fc8135a41aae.

--- test/index.web.ts
+++ test/index.web.ts
@@ -1,8 +1,30 @@
 import { LokiJsConnection } from "@js-soft/docdb-access-loki"
 import { WebLoggerFactory } from "@js-soft/web-logger"
-import { Test } from "./Test"
+import { Transport, TransportContext } from "@nmshd/transport"
+import { BackboneEnvironment, Test } from "./Test"
+
+const config = Test.config
+
+if (Transport.context === TransportContext.Web) {
+    switch (config.baseUrl) {
+        case BackboneEnvironment.Local:
+            config.baseUrl = "/svc-local"
+            break
+        case BackboneEnvironment.Dev:
+            config.baseUrl = "/svc-dev"
+            break
+        case BackboneEnvironment.Stage:
+            config.baseUrl = "/svc-stage"
+            break
+        case BackboneEnvironment.Prod:
+            config.baseUrl = "/svc-prod"
+            break
+        default:
+            throw new Error(`${config.baseUrl} is not a valid value for 'config.baseUrl'`)
+    }
+}
 
 const loggerFactory = new WebLoggerFactory()
 
 Test.runUnitTests(loggerFactory)
-Test.runIntegrationTests(Test.config, new LokiJsConnection("./db"), loggerFactory)
+Test.runIntegrationTests(config, new LokiJsConnection("./db"), loggerFactory)
