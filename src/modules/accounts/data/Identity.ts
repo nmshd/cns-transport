3be0c0cbd6d79ce122b3f49e6138eaf124a333f5
Stnmtz <t.stnmtz@outlook.de>
2021-12-28 17:06:37 +0100

    fix: use empty default values for now

--- src/modules/accounts/data/Identity.ts
+++ src/modules/accounts/data/Identity.ts
@@ -2,6 +2,7 @@ import { serialize, type, validate } from "@js-soft/ts-serval"
 import { CryptoSignaturePublicKey, ICryptoSignaturePublicKey } from "@nmshd/crypto"
 import { CoreSerializableAsync, ICoreSerializableAsync } from "../../../core/CoreSerializableAsync"
 import { CoreAddress } from "../../../core/types/CoreAddress"
+import { CoreDate } from "../../../core/types/CoreDate"
 
 export interface IIdentity extends ICoreSerializableAsync {
     address: CoreAddress
@@ -29,7 +30,42 @@ export class Identity extends CoreSerializableAsync implements IIdentity {
     @serialize()
     public realm: Realm
 
+    /**
+     * @deprecated
+     */
+    @validate({ nullable: true })
+    @serialize()
+    public name: string
+
+    /**
+     * @deprecated
+     */
+    @validate({ nullable: true })
+    @serialize()
+    public description: string
+
+    /**
+     * @deprecated
+     */
+    @validate({ nullable: true })
+    @serialize()
+    public createdAt: CoreDate
+
+    /**
+     * @deprecated
+     */
+    @validate({ nullable: true })
+    @serialize()
+    public type: string
+
     public static async from(value: IIdentity): Promise<Identity> {
-        return await super.fromT(value, Identity)
+        const identity = await super.fromT(value, Identity)
+
+        // TODO: Remove these default values once we're sure that nobody is accessing the deprecated properties anymore
+        identity.name = ""
+        identity.description = ""
+        identity.type = "unknown"
+        identity.createdAt = CoreDate.from("2020-01-01T00:00:00Z")
+        return identity
     }
 }
