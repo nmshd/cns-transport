d926ace865b9799f0fdc997dfeefe087f158fa26
Thomas Steinmetz <40369439+stnmtz@users.noreply.github.com>
2021-12-28 17:35:16 +0100

    Bugfix/make identity properties optional (#15)
    * chore: 1.1.3
    
    * fix: use empty default values for now
    
    * test: deprecated identity data

--- test/modules/account/AccountController.test.ts
+++ test/modules/account/AccountController.test.ts
@@ -1,4 +1,5 @@
-import { AccountController, Transport } from "@nmshd/transport"
+import { AccountController, Identity, Transport } from "@nmshd/transport"
+import { expect } from "chai"
 import { AbstractTest, TestUtil } from "../../testHelpers"
 
 export class AccountControllerTest extends AbstractTest {
@@ -25,6 +26,26 @@ export class AccountControllerTest extends AbstractTest {
             it("should init a second time", async function () {
                 await account.init()
             }).timeout(15000)
+
+            it("should return default values for empty identity properties", function () {
+                const id = account.identity.identity.toJSON() as any
+                expect(id.name).eq("")
+                expect(id.description).eq("")
+                expect(id.createdAt).eq("2020-01-01T00:00:00.000Z")
+                expect(id.type).eq("unknown")
+                const identity = Identity.from(id)
+                expect(identity).to.exist
+            })
+
+            it("should be able to read in Identity without deprecated data", function () {
+                const id = account.identity.identity.toJSON() as any
+                delete id.name
+                delete id.description
+                delete id.createdAt
+                delete id.type
+                const identity = Identity.from(id)
+                expect(identity).to.exist
+            })
 
             after(async function () {
                 await account.close()
