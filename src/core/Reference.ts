b1b457f47961b8ea5c429ff3397eeb215eb822fb
jkoenig <julian.koenig@js-soft.com>
2022-06-30 17:14:22 +0200

    refactor: let references extend Reference

---
+++ src/core/Reference.ts
@@ -0,0 +1,58 @@
+import { ISerializable, Serializable, serialize, validate } from "@js-soft/ts-serval"
+import { CoreBuffer, CryptoSecretKey, ICryptoSecretKey } from "@nmshd/crypto"
+import { TransportErrors } from "./TransportErrors"
+import { CoreId, ICoreId } from "./types/CoreId"
+
+export interface IReference extends ISerializable {
+    id: ICoreId
+    key: ICryptoSecretKey
+}
+
+export class Reference extends Serializable implements IReference {
+    @validate()
+    @serialize()
+    public id: CoreId
+
+    @validate()
+    @serialize()
+    public key: CryptoSecretKey
+
+    public truncate(): string {
+        const truncatedReference = CoreBuffer.fromUtf8(
+            `${this.id.toString()}|${this.key.algorithm}|${this.key.secretKey.toBase64URL()}`
+        )
+        return truncatedReference.toBase64URL()
+    }
+
+    public static fromTruncated(value: string): Reference {
+        const truncatedBuffer = CoreBuffer.fromBase64URL(value)
+        const splitted = truncatedBuffer.toUtf8().split("|")
+
+        if (splitted.length !== 3) {
+            throw TransportErrors.files.invalidTruncatedReference()
+        }
+
+        try {
+            const id = CoreId.from(splitted[0])
+            const alg = parseInt(splitted[1])
+            const key = splitted[2]
+            const secretKey = CryptoSecretKey.from({
+                algorithm: alg,
+                secretKey: CoreBuffer.fromBase64URL(key)
+            })
+
+            return Reference.from({
+                id: CoreId.from(id),
+                key: secretKey
+            })
+        } catch (e) {
+            throw TransportErrors.files.invalidTruncatedReference()
+        }
+    }
+
+    public static from(value: IReference | string): Reference {
+        if (typeof value === "string") return this.fromTruncated(value)
+
+        return this.fromAny(value)
+    }
+}
--- src/modules/files/transmission/FileReference.ts
+++ src/modules/files/transmission/FileReference.ts
@@ -1,56 +1,19 @@
-import { serialize, type, validate } from "@js-soft/ts-serval"
-import { CoreBuffer, CryptoSecretKey, ICryptoSecretKey } from "@nmshd/crypto"
-import { CoreId, CoreSerializable, ICoreSerializable, TransportErrors } from "../../../core"
+import { type, ValidationError } from "@js-soft/ts-serval"
+import { BackboneIds, IReference, Reference } from "../../../core"
 
-export interface IFileReference extends ICoreSerializable {
-    id: CoreId
-    key: ICryptoSecretKey
-}
+export interface IFileReference extends IReference {}
 
 @type("FileReference")
-export class FileReference extends CoreSerializable implements IFileReference {
-    @validate()
-    @serialize()
-    public id: CoreId
-
-    @validate()
-    @serialize()
-    public key: CryptoSecretKey
-
-    public truncate(): string {
-        const truncatedReference = CoreBuffer.fromUtf8(
-            `${this.id.toString()}|${this.key.algorithm}|${this.key.secretKey.toBase64URL()}`
-        )
-        return truncatedReference.toBase64URL()
-    }
-
-    public static fromTruncated(value: string): FileReference {
-        const truncatedBuffer = CoreBuffer.fromBase64URL(value)
-        const splitted = truncatedBuffer.toUtf8().split("|")
-
-        if (splitted.length !== 3) {
-            throw TransportErrors.files.invalidTruncatedReference()
+export class FileReference extends Reference implements IFileReference {
+    protected static override preFrom(value: any): any {
+        if (value?.id && BackboneIds.file.validate(value.id)) {
+            throw new ValidationError(FileReference.name, "id", "id must start with `FIL`")
         }
 
-        try {
-            const id = CoreId.from(splitted[0])
-            const alg = parseInt(splitted[1])
-            const key = splitted[2]
-            const secretKey = CryptoSecretKey.from({
-                algorithm: alg,
-                secretKey: CoreBuffer.fromBase64URL(key)
-            })
-
-            return FileReference.from({
-                id: CoreId.from(id),
-                key: secretKey
-            })
-        } catch (e) {
-            throw TransportErrors.files.invalidTruncatedReference()
-        }
+        return value
     }
 
-    public static from(value: IFileReference | string): FileReference {
-        return this.fromAny(value)
+    public static override from(value: IFileReference | string): FileReference {
+        return super.from(value)
     }
 }
