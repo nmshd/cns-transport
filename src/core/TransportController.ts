541e4de7707c2ccee8d85b92f7bdbf77e40b7690
jkoenig <julian.koenig@js-soft.com>
2022-07-20 15:33:11 +0200

    refactor: cleanup transport controller

--- src/core/TransportController.ts
+++ src/core/TransportController.ts
@@ -1,5 +1,6 @@
 import { IDatabaseCollectionProvider } from "@js-soft/docdb-access-abstractions"
 import { ILogger } from "@js-soft/logging-abstractions"
+import { EventBus } from "@js-soft/ts-utils"
 import { AccountController } from "../modules/accounts/AccountController"
 import { CoreSerializable } from "./CoreSerializable"
 import { IConfig, Transport } from "./Transport"
@@ -32,51 +33,47 @@ export enum ControllerName {
 }
 
 export class TransportController {
+    protected _initialized = false
+    public get initialized(): boolean {
+        return this._initialized
+    }
+
+    protected _dbClosed = false
+
     protected _log: ILogger
     public get log(): ILogger {
         return this._log
     }
 
-    protected _config: IConfig
-    public get config(): IConfig {
-        return this._config
-    }
-
-    protected _db: IDatabaseCollectionProvider
-    public get db(): IDatabaseCollectionProvider {
-        return this._db
-    }
-
-    protected _dbClosed = false
-
-    protected _transport: Transport
-    public get transport(): Transport {
-        return this._transport
-    }
-
-    protected _parent: AccountController
     public get parent(): AccountController {
         return this._parent
     }
 
-    protected _initialized: boolean
-    public get initialized(): boolean {
-        return this._initialized
+    public get config(): IConfig {
+        return this._parent.config
     }
 
-    protected _controllerName: ControllerName
+    public get db(): IDatabaseCollectionProvider {
+        return this._parent.db
+    }
+
+    public get transport(): Transport {
+        return this._parent.transport
+    }
+
     public get controllerName(): ControllerName {
         return this._controllerName
     }
 
-    public constructor(controllerName: ControllerName, parent: AccountController) {
-        this._controllerName = controllerName
-        this._transport = parent.transport
-        this._parent = parent
-        this._config = parent.config
-        this._db = parent.db
-        this._initialized = false
-        let loggerName: string = controllerName
+    public get eventBus(): EventBus {
+        return this.transport.eventBus
+    }
+
+    public constructor(
+        protected readonly _controllerName: ControllerName,
+        protected readonly _parent: AccountController
+    ) {
+        let loggerName: string = _controllerName
         if (this.config.debug && this.parent.activeDeviceOrUndefined?.deviceOrUndefined) {
             loggerName += ` of ${this.parent.activeDevice.device.id}`
         }
